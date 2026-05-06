89583d460165a996f7c8e9c03a8c758b9c1a8e9e
Akshay Khalatkar <kakshay@qti.qualcomm.com>
2026-05-06 08:15:41 +0530

    copyright-cleanups
    Signed-off-by: Akshay Khalatkar <kakshay@qti.qualcomm.com>

--- qcperf/backends/utils/wos/cpu-thermal-etw/inc/cpu_thermal_etw_library.h
+++ qcperf/backends/utils/wos/cpu-thermal-etw/inc/cpu_thermal_etw_library.h
@@ -1,20 +1,31 @@
-//-----------------------------------------------------------------------------
-//
-// Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries. All rights reserved.
-// Confidential and Proprietary - Qualcomm Technologies, Inc. and/or its subsidiaries.
-//
-// All data and information contained in or disclosed by this document are
-// confidential and proprietary information of Qualcomm Technologies, Inc., and
-// all rights therein are expressly reserved. By accepting this material, the
-// recipient agrees that this material and the information contained therein
-// are held in confidence and in trust and will not be used, copied, reproduced
-// in whole or in part, nor its contents revealed in any manner to others
-// without the express written permission of Qualcomm Technologies, Inc.
-//
-// This software may be subject to U.S. and international export, re-export, or
-// transfer ("export") laws.  Diversion contrary to U.S. and international law
-// is strictly prohibited.
-//-----------------------------------------------------------------------------
+/*
+        Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+        Redistribution and use in source and binary forms, with or without
+        modification, are permitted (subject to the limitations in the
+        disclaimer below) provided that the following conditions are met:
+                * Redistributions of source code must retain the above copyright
+                  notice, this list of conditions and the following disclaimer.
+                * Redistributions in binary form must reproduce the above
+                  copyright notice, this list of conditions and the following
+                  disclaimer in the documentation and/or other materials provided
+                  with the distribution.
+                * Neither the name of Qualcomm Technologies, Inc. nor the names of its
+                  contributors may be used to endorse or promote products derived
+                  from this software without specific prior written permission.
+        NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+        GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+        HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+        WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+        MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+        IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+        ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+        DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+        GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+        INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+        IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+        OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+        IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 /**
  * @file cpu_thermal_etw_library.h
